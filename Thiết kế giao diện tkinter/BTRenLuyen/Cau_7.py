036ef21952a9ccca71b384a2633b7228ec3335b9
MinhHoang1819 <115516411+MinhHoang1819@users.noreply.github.com>
2024-02-03 15:39:18 +0700

    Chuyển năm dương -> năm âm

--- Thiết kế giao diện tkinter/BTRenLuyen/Cau_7.py
+++ Thiết kế giao diện tkinter/BTRenLuyen/Cau_7.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
-def Nhap():
-    nam_duong.set('')
-    nam_am.set('')
+def Chuyen():
+    duong = int(nam_duong.get())
+    a = ['Canh', 'Tân', 'Nhâm', 'Quý', 'Giáp', 'Ất', 'Bính', 'Đinh', 'Mậu', 'Kỷ']
+    b = ['Tí', 'Sửu', 'Dần', 'Mẹo', 'Thìn', 'Tị', 'Ngọ', 'Mùi', 'Thân', 'Dậu', 'Tuất', 'Hợi']
+    nam_am.set(f'{a[duong%10]} {b[duong%12]}')
 
 root = Tk()
 root.title('Chuyển năm')
@@ -15,7 +17,7 @@ nam_am = StringVar()
 Label(root, text='Nhập năm dương:').grid(row=0, column=0)
 Entry(root, width=30, textvariable=nam_duong).grid(row=0, column=1)
 
-Button(root, text='Chuyển').grid(row=1, column=1)
+Button(root, text='Chuyển', command=Chuyen).grid(row=1, column=1)
 
 Label(root, text='Năm âm:').grid(row=2, column=0)
 Entry(root, width=30, textvariable=nam_am).grid(row=2, column=1)
